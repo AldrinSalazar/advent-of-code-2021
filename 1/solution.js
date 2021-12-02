2e1cf9181b40f6d390456f33f3d7d93b8afcd098
Aldrin Salazar <aldrin.salazar@finastra.com>
2021-12-02 08:11:17 -0400

    1

---
+++ 1/solution.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("input.txt").toString();
+const inputSplit = input.split("\n");
+//Part 1
+let solution1 = 0;
+inputSplit.reduce((prev, curr) => {
+  solution1 += curr > prev;
+  return curr;
+}, 0);
+
+console.log(solution1);
+
+// Part 2
+let solution2 = 0;
+let sums = [];
+for (let i = 1; i < inputSplit.length - 1; i++) {
+  sums.push(inputSplit[i - 1] + inputSplit[i] + inputSplit[i + 1]);
+}
+
+sums.reduce((prev, curr) => {
+  solution2 += curr > prev;
+  return curr;
+}, 0);
+
+console.log(solution2 - 1);
